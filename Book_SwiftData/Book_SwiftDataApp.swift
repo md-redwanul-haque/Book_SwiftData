efc9010cb8fc2babcdd07f5de09ffd8952c3463d
md-redwanul-haque <redwan229900@gmail.com>
2024-11-02 11:48:31 +0600

    Initial Commit

---
+++ Book_SwiftData/Book_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_SwiftDataApp.swift
+//  Book_SwiftData
+//
+//  Created by Redwan on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Book_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Book_SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Book_SwiftData
+//
+//  Created by Redwan on 2/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
