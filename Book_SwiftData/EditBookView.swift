0e87d67adaed4c867fa8306c1d31f3e9361df781
md-redwanul-haque <redwan229900@gmail.com>
2024-11-03 15:37:51 +0600

    made a screen for if list data empth , and create read and delete done

---
+++ Book_SwiftData/EditBookView.swift
@@ -0,0 +1,18 @@
+//
+//  EditBookView.swift
+//  Book_SwiftData
+//
+//  Created by Redwan on 3/11/24.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditBookView()
+}
